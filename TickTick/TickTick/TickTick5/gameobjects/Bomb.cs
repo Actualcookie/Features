aa0d7006c0d9855e252873713df33ebc40019f3a
svenosman <sven95zon@gmail.com>
2014-11-02 14:08:25 +0100

    Revert "raketten gaan dood"
    This reverts commit 518e997e0d90f90629d38b256ef92ce3b8638d30.

--- TickTick/TickTick/TickTick5/gameobjects/Bomb.cs
+++ TickTick/TickTick/TickTick5/gameobjects/Bomb.cs
@@ -12,6 +12,6 @@ class Bomb: AnimatedGameObject
     {
         soundeffect = Content.Load<SoundEffect>("kaboom");
 
-   }
+    }
 
-   }
+    }
--- TickTick/TickTick/TickTick5/gameobjects/enemies/Rocket.cs
+++ TickTick/TickTick/TickTick5/gameobjects/enemies/Rocket.cs
@@ -45,14 +45,17 @@ class Rocket : AnimatedGameObject
     {
         Player player = GameWorld.Find("player") as Player;
         if (this.CollidesWith(player) && this.Visible)
-        {
-            if (player.Position.Y + 20 < this.position.Y)
-            {
-                this.Reset();
-            }
-            else player.Die(false);
-        }
-            
+<<<<<<< HEAD
+            Rocketjump();
+=======
+            player.Die(false);
+
+>>>>>>> origin/master
+    }
+
+    public void Rocketjump()
+    {
+        
     }
     
 }
